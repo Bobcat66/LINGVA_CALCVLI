672e0284598a2c3e073b32903e58cbcfc88c0346
Jesse Kane <jessekane66@gmail.com>
2024-02-25 19:33:54 -0500

    c

--- LINGVA_CALCVLI_EXECVTER.py
+++ LINGVA_CALCVLI_EXECVTER.py
@@ -3,16 +3,31 @@ from LINGVA_CALCVLI_PARSER import parser
 class compiler():
     def __init__(self) -> None:
         self.parser = parser
+            
     def printTree(self,statement):
         #breaks down code and makes it more readable
-        if statement[0][0] == '@':
-            print(statement)
-            return
-        else:
-            print(statement[0],end=" ")
+        if statement[0] == '$WHILE':
+            print('WHILE',statement[1])
+            print('{')
+            for ele in statement[2]:
+                #print(ele)
+                self.printTree(ele)
+            print('}')
+        elif statement[0] == '$IF':
+            for ele in statement:
+                print(ele[0])
+                print('{')
+                for ele2 in ele[1]:
+                    self.printTree(ele2)
+                print('}')
+        elif statement[0][0] == '$':
+            print(statement[0][1:], end=" ")
             for ele in statement[1:]:
                 self.printTree(ele)
-            return
+            print()
+        else:
+            print(statement, end=" ")
+
     def compile(self,code):
         parsedCode = self.parser.parse(code)
         for statement in parsedCode:
@@ -21,17 +36,20 @@ class compiler():
 compiler = compiler()
 
 compiler.compile("""IMPERIVM MEVM INVOCO ET PRAECIPIO TIBI
-DICERE 'SALVE MVNDI'
-SI VERVM TVNC
-DICERE 'op one'
-SI VERVM TVNC
-DICERE 'hello'
-FINIS FINIS_CIRCVITVS
-FINIS SIN VERVM TVNC
-DICERE 'op two'
-FINIS ALITER TVNC
-DICERE 'op three'
+DECLARO COVNTER NVMERVS
+ASSIGNO COVNTER NO. I
+ORDO_DECLARO ARRAY NO. V NVMERVS
+ORDO_IMMVTO ARRAY NO. I NO. I
+ORDO_IMMVTO ARRAY NO. II NO. II
+ORDO_IMMVTO ARRAY NO. III NO. III
+ORDO_IMMVTO ARRAY NO. IV NO. IV
+ORDO_IMMVTO ARRAY NO. V NO. V
+DVM MINOR_VP COVNTER NO. V TVNC
+DICERE IMAGO EXPROMO ARRAY COVNTER SCRIPTVM
+INCREMENTVM COVNTER
 FINIS
 FINIS_CIRCVITVS
 CETERVM AVTEM CENSEO CARTHAGINEM ESSE DELENDAM
 """)
+
+compiler.compile("""""")
